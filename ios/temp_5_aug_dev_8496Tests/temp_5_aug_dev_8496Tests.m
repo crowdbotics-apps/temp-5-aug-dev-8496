8f383341a1a59033536fc9417a23911dd14fbf54
Crowdbotics <team@crowdbotics.com>
2020-08-05 08:18:05 +0000

    Initial commit.

---
+++ ios/temp_5_aug_dev_8496Tests/temp_5_aug_dev_8496Tests.m
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
+
+@interface temp_5_aug_dev_8496 : XCTestCase
+
+@end
+
+@implementation temp_5_aug_dev_8496
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
